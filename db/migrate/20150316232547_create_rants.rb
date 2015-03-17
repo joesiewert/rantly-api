09e6a11c614f8e481281a4c72b996536fe74bd0f
joesiewert <joe.siewert@gmail.com>
2015-03-16 18:30:26 -0600

    Migration for rants

---
+++ db/migrate/20150316232547_create_rants.rb
@@ -0,0 +1,10 @@
+class CreateRants < ActiveRecord::Migration
+  def change
+    create_table :rants do |t|
+      t.string :title
+      t.text :body
+      t.belongs_to :user
+      t.timestamps
+    end
+  end
+end
