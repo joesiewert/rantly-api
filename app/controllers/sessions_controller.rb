73cade6295346324753f251ee4093d255f324261
joesiewert <joe.siewert@gmail.com>
2015-03-21 21:18:13 -0600

    More Devise setup

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < Devise::SessionsController
+  respond_to :json
+
+  def create
+    super do |user|
+      data = {
+        email: user.email,
+        token: user.authentication_token
+      }
+      render json: data, status: 201 and return
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :rants, only: [:index, :create, :update, :destroy]
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'sessions'}
 end
