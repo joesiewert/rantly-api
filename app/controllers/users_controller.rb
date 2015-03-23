9da70d623ba62c928254f34c9212f36606fd0e0b
joesiewert <joe.siewert@gmail.com>
2015-03-22 18:03:11 -0600

    Setup user create route

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < Devise::RegistrationsController
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :name, :email, :password)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :rants, only: [:index, :create, :update, :destroy]
-  devise_for :users, controllers: {sessions: 'sessions'}
+  devise_for :users, controllers: {sessions: 'sessions', registrations: 'users'},
+    skip: [:sessions, :passwords, :registrations]
+
+  as :user do
+    post '/users/sign_in' => 'sessions#create', as: :user_session
+    post '/users' => 'users#create', as: :user_registration
+  end
 end
